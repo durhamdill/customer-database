21c1fdf79b13a7c8b5f739497f1d041fae27ea56
Katy Dillard <durhamdill@gmail.com>
2017-08-02 16:04:58 -0400

    formatting/add loop

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 //console.log(customers);
+let customerBuild = () => {
+//
+  let customerGrid = document.querySelector(".customerGrid");
+//
+  for (let x=0; x<customers.results.length; x++){
+//
+// // ES6 function format: let multiply = (0,y) => {return 0*y};
 
-let customerProfile = `
-  <div>
-    <img src="${customers.results[0].picture.large}">
-    <h1 class="name">${customers.results[0].name.first} ${customers.results[0].name.last}</h1>
-    <a href="mailto:${customers.results[0].email}">${customers.results[0].email}</a>
-    <p class="address">${customers.results[0].location.city}, ${customers.results[0].location.state} ${customers.results[0].location.postcode}</p>
-    <p class="phone">${customers.results[0].phone}</p>
-    <p class="ssn">${customers.results[0].id.value}</p>
+    let customerProfile = `
+      <div class="customerBox">
+        <img src="${customers.results[x].picture.large}">
+        <h1 class="name">${customers.results[x].name.first} ${customers.results[x].name.last}</h1>
+        <a class = "email" href="mailto:${customers.results[x].email}">${customers.results[0].email}</a>
+        <p class="address">${customers.results[x].location.street}</p>
+        <p class="address">${customers.results[x].location.city}, ${customers.results[x].location.state} ${customers.results[x].location.postcode}</p>
+        <p class="phone">${customers.results[x].phone}</p>
+        <p class="ssn">${customers.results[x].id.value}</p>
+      </div>
+    `;
 
-  </div>
+document.querySelector('.customerGrid').innerHTML += customerProfile;
+}
+}
 
-`;
-
-document.querySelector('.customerGrid').innerHTML = customerProfile;
-
-
+customerBuild();
+//
 //attempts to reformat phone number:
 
 //placeholder="555-55-5555" pattern="\d{3}-?\d{2}-?\d{4}"
