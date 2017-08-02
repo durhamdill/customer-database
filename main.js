efe164ea608540ce50ea93b962a21fa5d8e76921
Katy Dillard <durhamdill@gmail.com>
2017-08-02 13:56:51 -0400

    build initial template for customer profile

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log(customers);
+//console.log(customers);
+
+let customerProfile = `
+  <div>
+    <img src="${customers.results[0].picture.large}">
+    <h1 class="name">${customers.results[0].name.first} ${customers.results[0].name.last}</h1>
+    <a href="mailto:${customers.results[0].email}">${customers.results[0].email}</a>
+    <p class="address">${customers.results[0].location.city}, ${customers.results[0].location.state} ${customers.results[0].location.postcode}</p>
+    <p class="phone">${customers.results[0].phone}</p>
+    <p class="ssn">${customers.results[0].id.value}</p>
+
+  </div>
+
+`;
+
+document.querySelector('.customerGrid').innerHTML = customerProfile;
+
+
+//attempts to reformat phone number:
+
+//placeholder="555-55-5555" pattern="\d{3}-?\d{2}-?\d{4}"
+//.val().replace(/[\s().+-]/g, '');
